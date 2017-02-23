bb104bffdbc210908d1f4d75b43b850d0765e3d1
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2017-02-23 14:30:41 +0100

    fixes module tests

--- test/module/exports.js
+++ test/module/exports.js
@@ -1,73 +1,51 @@
 /* global test */
-
-const {ok, equal, deepEqual} = require('assert')
-
 const WebSocket = require('ws')
-const Server = require('../..')
+const {ok, equal} = require('assert')
 
-test('module/exports', function () {
-  test('is callable', isFunction(Server))
-  test(`inherits from ${Server.name}`, function () {
-    const {Server: {name}} = require('ws')
-    equal(Server.name, name)
+test('module.exports', function () {
+  const Server = require('../..')
+
+  test('is callable', function () {
+    equal(typeof Server, 'function')
   })
+
+  test(`inherits from ${WebSocket.Server.name}`, function () { // legacy is ok
+    equal(Server.name, WebSocket.Server.name)
+  })
+
   test('.createServerFrom(net.Server)', function () {
-    test('is callable', isFunction(Server.createServerFrom))
-    test('attaches to net.Server', function () {
-      const {Server: {name}} = require('ws')
-      const {createServer} = require('http')
-      const server = Server.createServerFrom(createServer())
-      equal(server.constructor.name, name)
-    })
-    test('returns instance of ws.Server', function () {
-      const {createServer} = require('http')
-      const server = Server.createServerFrom(createServer())
-      ok(server instanceof require('ws').Server)
-    })
-    // test('attaches to connectionListener', function(done) {
-    // })
-    test('wraps http.Server', function (done) {
-      const {createServer} = require('http')
-      const http = createServer().listen(0, onceListening)
-      Server.createServerFrom(http).on('connection', (ws) => ws.close())
-      function onceListening () {
-        const {address: host, port} = this.address()
-        new WebSocket(`ws://${host}:${port}`)
-          .on('close', () => this.close(() => done()))
-      }
-    })
-  })
-  test('.createServer([Function: connectionListener])', function () {
-    test('is callable', isFunction(Server.createServer))
-    test('creates http.Server', function () {
-      ok(Server.createServer() instanceof require('http').Server)
-    })
-  //   const {createServer} = WebSocketServer
-  //   test('is callable', isFunction(createServer))
-  //   test('connectionListener is bound to 'connection' event', function (done) {
-  //     WebSocketServer
-  //       .createServer(Function.prototype)
-  //       .listen(0, function onceListening () {
-  //         console.log(this)
-  //         this.close()
-  //         done()
-  //       })
-  //   })
-  //   // test.skip('createServer()')
-  //   // test.skip('createServer(connectionListener)')
-  //   // test.skip('createServer(options, connectionListener)')
-  })
-  // test('.createCluster([options[, connectionListener]])', function () {
-  //   test('is callable', isFunction(WebSocketServer.createCluster))
-  //   // test.skip('createCluster()')
-  //   // test.skip('createCluster(connectionListener)')
-  //   // test.skip('createCluster(options, connectionListener)')
-  // })
-})
+    const {createServerFrom} = require('../..')
 
-function isFunction (type) {
-  return function (done) {
-    equal(typeof type, 'function', `expected ${type} to be callable`)
-    done(null)
-  }
-}
+    test('is callable', function () {
+      equal(typeof createServerFrom, 'function')
+    })
+
+    test('forks net.Server', function (done) {
+      const {createServer} = require('net')
+      const server = createServer().listen(0, close(done))
+      ok(createServerFrom(server) instanceof WebSocket.Server)
+    })
+
+    test('forks http.Server', function (done) {
+      const {createServer} = require('http')
+      const server = createServer().listen(0, close(done))
+      ok(createServerFrom(server) instanceof WebSocket.Server)
+    })
+
+    test('forks https.Server', function (done) {
+      const {createCertificate} = require('pem')
+      const {createServer} = require('https')
+      createCertificate({days: 1, selfSigned: true}, (err, {serviceKey: key, certificate: cert}) => {
+        if (err) done(err)
+        const server = createServer({key, cert}).listen(0, close(done))
+        ok(createServerFrom(server) instanceof WebSocket.Server)
+      })
+    })
+
+    function close (done) {
+      return function () {
+        this.close(done)
+      }
+    }
+  })
+})
